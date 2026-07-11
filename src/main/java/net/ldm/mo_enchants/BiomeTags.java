8b692fe660ec6b755e2901ecd613d717e170821e
Logan Dhillon <dev@logandhillon.com>
2026-07-10 22:08:59 -0400

    perf: removed unused fields in entrypoint

--- src/main/java/net/ldm/mo_enchants/BiomeTags.java
+++ src/main/java/net/ldm/mo_enchants/BiomeTags.java
@@ -6,6 +6,6 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 
 public class BiomeTags {
-    public static TagKey<Biome> IS_FROZEN = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MODID, "is_frozen"));
-    public static TagKey<Biome> IS_HOT = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MODID, "is_hot"));
+    public static TagKey<Biome> IS_FROZEN = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MOD_ID, "is_frozen"));
+    public static TagKey<Biome> IS_HOT = TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(MoEnchantsMod.MOD_ID, "is_hot"));
 }
