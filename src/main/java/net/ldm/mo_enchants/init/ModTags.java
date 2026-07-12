a63f382371750606f42ddfe4b3a18f3dc182da6d
Logan Dhillon <dev@logandhillon.com>
2026-07-12 17:09:24 -0400

    fix: use c:is_cold/is_hot tags for curses, delete biome tags

--- src/main/java/net/ldm/mo_enchants/init/ModTags.java
+++ src/main/java/net/ldm/mo_enchants/init/ModTags.java
@@ -7,12 +7,8 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.biome.Biome;
 
 public class ModTags {
-    public static final TagKey<Biome> IS_FROZEN = tag(Registries.BIOME, "is_frozen");
-    public static final TagKey<Biome> IS_HOT    = tag(Registries.BIOME, "is_hot");
-
     public static final TagKey<EntityType<?>> BOSSES = tag(Registries.ENTITY_TYPE, "bosses");
 
     private static <T> TagKey<T> tag(ResourceKey<? extends Registry<T>> type, String id) {
