7760f06b48963fd3b2b0b2b527c9ef627e08f442
Logan Dhillon <dev@logandhillon.com>
2026-07-22 14:22:06 -0400

    feat: migrate GLMs to neoforge

--- src/main/java/net/ldm/mo_enchants/loot/SmeltingTouchLootModifier.java
+++ src/main/java/net/ldm/mo_enchants/loot/SmeltingTouchLootModifier.java
@@ -1,21 +1,22 @@
 package net.ldm.mo_enchants.loot;
 
-import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.ldm.mo_enchants.init.ModEnchantments;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.item.crafting.SmeltingRecipe;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraftforge.common.loot.IGlobalLootModifier;
-import net.minecraftforge.common.loot.LootModifier;
+import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
+import net.neoforged.neoforge.common.loot.LootModifier;
 
 import java.util.Optional;
 
@@ -23,7 +24,7 @@ import java.util.Optional;
  * @author Logan Dhillon
  */
 public class SmeltingTouchLootModifier extends LootModifier {
-    public static final Codec<SmeltingTouchLootModifier> CODEC = RecordCodecBuilder.create(
+    public static final MapCodec<SmeltingTouchLootModifier> CODEC = RecordCodecBuilder.mapCodec(
             inst -> codecStart(inst).apply(inst, SmeltingTouchLootModifier::new));
 
     public SmeltingTouchLootModifier(LootItemCondition[] conditionsIn) {
@@ -36,18 +37,18 @@ public class SmeltingTouchLootModifier extends LootModifier {
 
         // if not a player or player's tool doesn't have smelting touch
         if (!(entity instanceof Player player) || EnchantmentHelper.getTagEnchantmentLevel(
-                ModEnchantments.SMELTING_TOUCH.get(), player.getMainHandItem()) <= 0)
+                ModEnchantments.SMELTING_TOUCH, player.getMainHandItem()) <= 0)
             return loot;
 
         for (int i = 0; i < loot.size(); i++) {
             ItemStack stack = loot.get(i);
 
-            Optional<SmeltingRecipe> recipe = player.level().getRecipeManager().getRecipeFor(
-                    RecipeType.SMELTING, new SimpleContainer(stack), player.level());
+            Optional<RecipeHolder<SmeltingRecipe>> recipe = player.level().getRecipeManager().getRecipeFor(
+                    RecipeType.SMELTING, new SingleRecipeInput(stack), player.level());
 
             int finalI = i;
             recipe.ifPresent(r -> {
-                ItemStack result = r.getResultItem(player.level().registryAccess()).copy();
+                ItemStack result = r.value().getResultItem(player.level().registryAccess()).copy();
                 result.setCount(result.getCount() * stack.getCount());
                 loot.set(finalI, result);
             });
@@ -57,7 +58,7 @@ public class SmeltingTouchLootModifier extends LootModifier {
     }
 
     @Override
-    public Codec<? extends IGlobalLootModifier> codec() {
+    public MapCodec<? extends IGlobalLootModifier> codec() {
         return CODEC;
     }
 }
